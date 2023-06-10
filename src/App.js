5fe6eaddb359ccce87e653d4ce87775060b42579
martinyis <martinyis11@gmail.com>
2023-06-10 11:52:21 -0700

    Tailwind CSS test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import "./App.css";
-
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="container mx-auto">
+      <p className=" font-bold mb-5 text-center md:text-xl lg:text-2xl xl:text-3xl">
+        Welcome!
+      </p>
+      <p className="text-red-400 text-lg text-center">
+        React and Tailwind CSS in action
+      </p>
+    </div>
+  );
 }
-
 export default App;
