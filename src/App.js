00204ddea9ae56b9b81a67c15afcb03687af22e4
martinyis <martinyis11@gmail.com>
2023-06-11 12:24:35 -0700

    Folder Structure SetUp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,4 @@
 function App() {
-  return (
-    <div className="container mx-auto">
-      <p className=" font-bold mb-5 text-center md:text-xl lg:text-2xl xl:text-3xl">
-        Welcome!
-      </p>
-      <p className="text-red-400 text-lg text-center">
-        React and Tailwind CSS in action
-      </p>
-    </div>
-  );
+  return <div className="container mx-auto"></div>;
 }
 export default App;
