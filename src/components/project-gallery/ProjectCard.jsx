87ccd83522fdde161488fbbdc81061ba1526b604
rawasaditya <rawasaditya@gmail.com>
2023-06-16 08:01:18 +0530

    Overflowing Tech Stack Bar in Developer Card for Small Screen Sizes #10

--- src/components/project-gallery/ProjectCard.jsx
+++ src/components/project-gallery/ProjectCard.jsx
@@ -21,8 +21,8 @@ const ProjectCard = () => {
           isMobile ? "flex flex-col" : "flex justify-between"
         } pt-[18px] items-center pb-[5px]`}
       >
-        <h3 className="text-2xl text-white font-bold">RepoAppIdea</h3>
-        <div className="flex gap-x-2 items-center justify-center">
+        <h3 className="text-2xl font-bold text-white">RepoAppIdea</h3>
+        <div className="flex items-center justify-center gap-x-2">
           <img
             onClick={handleStar}
             className="mt-[-3px] cursor-pointer"
@@ -45,7 +45,7 @@ const ProjectCard = () => {
       </p>
       <div className="px-6 flex items-center pb-[25px]">
         <img src={devlook} alt="" />
-        <div className="pl-5 flex gap-x-4 gap-y-2 flex-wrap">
+        <div className="flex flex-wrap pl-5 gap-x-4 gap-y-2">
           <div className="w-[98px] h-[31px] text-white text-base rounded-[18px] bg-[#27374D] flex justify-center items-center">
             {" "}
             Back End
@@ -60,8 +60,8 @@ const ProjectCard = () => {
           </div>
         </div>
       </div>
-      <div className="h-[53px] bg-[#DDE6ED] rounded-t-lg rounded-b-[30px] flex items-center justify-center">
-        <div className="scrollbar flex gap-x-4 max-w-[634px] overflow-x-auto">
+      <div className="h-[53px] bg-[#DDE6ED] rounded-t-lg rounded-b-[30px] flex  items-center justify-center ">
+        <div className="scrollbar justify-center gap-1 flex md:gap-x-4 max-w-[634px] overflow-x-auto flex-wrap md:flex-nowrap">
           <div className="w-[100px] h-[19px] rounded-[30px] bg-[#1D267D] flex items-center justify-center text-[10px] ">
             JavaScript
           </div>
