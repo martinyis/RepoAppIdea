567f22efe00c07e804f22954668fb558502d3542
martinyis <martinyis11@gmail.com>
2023-06-20 14:38:52 -0700

    Change host name

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instanse = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 instanse.interceptors.request.use((config) => {
