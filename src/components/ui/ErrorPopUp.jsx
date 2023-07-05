8d7cd48f1b9f99b8a39b8db5d448070d28ef6d9f
Kusha-web <kushalmitra189@gmail.com>
2023-07-05 22:03:53 +0530

    error changes

--- src/components/ui/ErrorPopUp.jsx
+++ src/components/ui/ErrorPopUp.jsx
@@ -1,17 +1,30 @@
+import bgImg from "C:/Users/kusha/Desktop/Open Source Prjects/RepoAppIdea/src/assets/how-it-works-simplifying-data-transfer@2x.png"
+
 const ErrorPopUp = (props) => {
   const { error, handleTryAgain } = props;
   const handleClick = () => {
     handleTryAgain();
   };
+
+
   return (
-    <div className="absolute top-[200px] md:w-[50%] w-[80%] sm:h-[50%] h-[70%] bg-[#ffffff] text-[#ff0000] rounded-[50px] flex flex-col items-center justify-center gap-[80px] py-10 z-10">
-      <div className="text-2xl px-8 text-center">{error}</div>
-      <button
-        onClick={handleClick}
-        className="md:w-[20%] w-[40%] h-[10%] bg-slate-500 rounded-[26px] text-lg font-bold text-white transform hover:scale-125 transition duration-200"
-      >
-        Try Again
-      </button>
+    <div className="absolute top-[200px] md:w-[50%] w-[80%] sm:h-[50%] h-[70%] bg-[#ffffff] text-[#ff0000] rounded-[50px] flex md:flex-row flex-col items-center justify-center gap-[80px] py-20 z-10"
+      style={{ padding: "20px", gap: "10px" }}
+    >
+      <img className="md:h-[95%] h-[70%]" src={bgImg} style={{ objectFit: "cover" }} alt="" srcset="" />
+      <div style={{ textAlign: "center" }}>
+        <h1 style={{ fontSize: "4rem", color: "#334c6f", fontWeight: "bold" }}>Oops!</h1>
+        <div className="text-2xl text-center">{error}</div>
+        <button
+          onClick={handleClick}
+          style={{
+            marginTop: "10px"
+          }}
+          className="md:w-[40%] w-[40%] h-[15%] bg-slate-500 rounded-[26px] text-lg font-bold text-white transform hover:scale-125 transition duration-200"
+        >
+          Try Again
+        </button>
+      </div>
     </div>
   );
 };
