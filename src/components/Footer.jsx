12a429559cd5a81949632e85cfc6cc6c41126ba4
Kao Saelor <saelor.kao@gmail.com>
2023-07-13 23:20:56 -0700

    added mouse-hover on footer social icons and reorganize code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,15 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import bigLogo from "./../assets/logos/repoapp-logo.png";
 import smallLogo from "./../assets/logos/repo-app-small.png";
-import { useState, useEffect } from "react";
 import linkedIn from "./../assets/logos/linkedin-logo.png";
 import github from "./../assets/logos/github-logo.png";
 import google from "./../assets/logos/googl-logo.png";
-import { Link } from "react-router-dom";
+
 const Footer = () => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const logoSrc = windowWidth < 768 ? smallLogo : bigLogo;
+
   useEffect(() => {
     const handleResize = () => {
       setWindowWidth(window.innerWidth);
@@ -19,8 +21,9 @@ const Footer = () => {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
+
   return (
-    <div className=" flex flex-col items-center justify-center mb-[64px] absolute bottom-0 w-[100%]  left-1/2 transform -translate-x-1/2">
+    <div className="flex flex-col items-center justify-center mb-[64px] absolute bottom-0 w-[100%] left-1/2 transform -translate-x-1/2">
       <Link to="/" onClick={() => window.scrollTo(0, 0)}>
         <div className="pb-4">
           <img src={logoSrc} alt="" />
@@ -38,13 +41,28 @@ const Footer = () => {
         </li>
       </ul>
       <div className="flex gap-x-4 pb-10">
-        <a href="https://www.linkedin.com/in/martin-babak-116850244/">
+        <a
+          href="https://www.linkedin.com/in/martin-babak-116850244/"
+          style={{ transition: "transform 0.3s" }}
+          onMouseOver={(e) => (e.currentTarget.style.transform = "scale(1.2)")}
+          onMouseOut={(e) => (e.currentTarget.style.transform = "scale(1)")}
+        >
           <img src={linkedIn} alt="" />
         </a>
-        <a href="https://github.com/martinyis/RepoAppIdea">
+        <a
+          href="https://github.com/martinyis/RepoAppIdea"
+          style={{ transition: "transform 0.3s" }}
+          onMouseOver={(e) => (e.currentTarget.style.transform = "scale(1.2)")}
+          onMouseOut={(e) => (e.currentTarget.style.transform = "scale(1)")}
+        >
           <img src={github} alt="" />
         </a>
-        <a href="https://www.google.com/">
+        <a
+          href="https://www.google.com/"
+          style={{ transition: "transform 0.3s" }}
+          onMouseOver={(e) => (e.currentTarget.style.transform = "scale(1.2)")}
+          onMouseOut={(e) => (e.currentTarget.style.transform = "scale(1)")}
+        >
           <img src={google} alt="" />
         </a>
       </div>
